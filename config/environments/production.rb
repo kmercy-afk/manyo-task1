60ae8812754e38b5f4e3ab1007c06b353f23b340
Mercy Wanji <kmercyafk@gmail.com>
2026-02-11 16:33:11 +0300

    Disable encrypted secrets in production for Render deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.read_encrypted_secrets = true   # disabled for Render deploy
+  # config.read_encrypted_secrets = true   # disabled for Render deploy - no secrets needed
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
